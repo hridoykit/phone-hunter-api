260c20a7a0803e1c122fb2469b78862005da1d3d
hridoykit <hridoy.tsa@gmail.com>
2024-11-14 09:04:48 +0600

    fetch + show data

--- src/phone-hunter.js
+++ src/phone-hunter.js
@@ -1,4 +1,19 @@
+const fetchPhoneData = async () =>{
+    const data = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const phones = await data.json();
+    // console.log(phones.data)
+    showPhone(phones.data);
+};
 
+const showPhone = (phones) => {
+    const container = document.getElementById('phone-container');
+
+    phones.forEach(phone => {
+        const li = document.createElement('li');
+        li.innerText = phone.phone_name;
+        container.appendChild(li);
+    });
+}
 //     const container = document.getElementById('phone-container');
 //     for(const phone of phones){
 //         // console.log(phone.phone_name);
