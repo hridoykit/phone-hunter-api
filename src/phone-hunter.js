9a8d648be169c7389204b601fdbaf6b6c20815cb
hridoykit <hridoy.tsa@gmail.com>
2024-11-14 09:18:02 +0600

    build structure

---
+++ src/phone-hunter.js
@@ -0,0 +1,8 @@
+
+//     const container = document.getElementById('phone-container');
+//     for(const phone of phones){
+//         // console.log(phone.phone_name);
+//         const li = document.createElement('li');
+//         li.innerText = phone.phone_name;
+//         container.appendChild(li);
+//     }
