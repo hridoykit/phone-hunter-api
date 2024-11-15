6e22f8b1b2f20f5ffa0f589a4fc817a5d45d37a3
hridoykit <hridoy.tsa@gmail.com>
2024-11-16 01:13:40 +0600

    search phone by name+display phone details by card

--- src/phone-hunter.js
+++ src/phone-hunter.js
@@ -1,23 +1,42 @@
-const fetchPhoneData = async () =>{
-    const data = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const fetchPhoneData = async (searchText) => {
+    const data = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const phones = await data.json();
-    // console.log(phones.data)
     showPhone(phones.data);
 };
 
-const showPhone = (phones) => {
+const showPhone = phones => {
     const container = document.getElementById('phone-container');
 
+    // clear previous container items
+    container.textContent = "";
+
     phones.forEach(phone => {
-        const li = document.createElement('li');
-        li.innerText = phone.phone_name;
-        container.appendChild(li);
+        // const li = document.createElement('li');
+        // li.innerText = phone.phone_name;
+        // container.appendChild(li);
+        const card = document.createElement('div');
+        card.classList = `bg-gray-100 w-96 shadow-lg flex flex-col items-center justify-center rounded-lg p-4`;
+        card.innerHTML = `
+            <img
+                src="${phone.image}"
+                class="p-4"
+            />
+            <div class="items-center text-center space-y-2">
+                <h2 class="text-lg font-semibold">${phone.phone_name}</h2>
+                <p>Model: <span class="text-lime-400 font-semibold text-md">${phone.slug}</span></p>
+                <p>${phone.brand} brand</p>
+                <button class="btn btn-primary">Buy Now</button>
+            </div>
+            `;
+        container.appendChild(card);
     });
-}
-//     const container = document.getElementById('phone-container');
-//     for(const phone of phones){
-//         // console.log(phone.phone_name);
-//         const li = document.createElement('li');
-//         li.innerText = phone.phone_name;
-//         container.appendChild(li);
-//     }
+};
+
+const handleSearchField = () => {
+    const inputValue = document.getElementById('phone-search-field');
+    const value = inputValue.value;
+    fetchPhoneData(value);
+    
+    // clear the input field
+    document.getElementById('phone-search-field').value = "";
+};
