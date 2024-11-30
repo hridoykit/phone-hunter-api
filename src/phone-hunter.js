8d0df92dd140fa0ca162bd86e61b244913926f74
hridoykit <hridoy.tsa@gmail.com>
2024-11-30 14:30:23 +0600

    conditionally show all button + limit to 10 items

--- src/phone-hunter.js
+++ src/phone-hunter.js
@@ -1,16 +1,28 @@
-const fetchPhoneData = async (searchText) => {
-    const data = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
-    const phones = await data.json();
+const loadPhoneData = async (searchText) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
+    const phones = await res.json();
     showPhone(phones.data);
 };
 
-const showPhone = phones => {
+const showPhone = phones =>{
+    console.log(phones)
     const container = document.getElementById('phone-container');
 
-    // clear previous container items
+    // clear previous items of container
     container.textContent = "";
 
-    phones.forEach(phone => {
+    if(phones.length > 9){
+        document.getElementById('show-all-btn').classList.remove('hidden');
+    }
+    else{
+        document.getElementById('show-all-btn').classList.add('hidden');
+    };
+    
+    // limit to first 10 phones 
+    phones = phones.slice(0, 9);
+    console.log(phones);
+
+    phones.forEach(phone =>{
         // const li = document.createElement('li');
         // li.innerText = phone.phone_name;
         // container.appendChild(li);
@@ -23,20 +35,20 @@ const showPhone = phones => {
             />
             <div class="items-center text-center space-y-2">
                 <h2 class="text-lg font-semibold">${phone.phone_name}</h2>
-                <p>Model: <span class="text-lime-400 font-semibold text-md">${phone.slug}</span></p>
-                <p>${phone.brand} brand</p>
-                <button class="btn btn-primary">Buy Now</button>
+                <p>Model: <span class="font-bold text-md">${phone.slug}</span></p>
+                <p>${phone.brand} origin</p>
+                <button class="bg-lime-500 px-5 py-1 rounded-lg text-white font-bold">Buy Now</button>
             </div>
             `;
         container.appendChild(card);
     });
 };
 
-const handleSearchField = () => {
-    const inputValue = document.getElementById('phone-search-field');
-    const value = inputValue.value;
-    fetchPhoneData(value);
-    
-    // clear the input field
+const handleSearchField = () =>{
+    const inputValue = document.getElementById('phone-search-field').value;
+    // console.log(inputValue);
+    loadPhoneData(inputValue);
+
+    // clear input field
     document.getElementById('phone-search-field').value = "";
 };
